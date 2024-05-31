2ece3edeaad8c466265e289be2261884d0430323
ian-bulovic <ian.bulovic@gmail.com>
2024-05-31 16:18:02 -0400

    reorganize page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,46 @@ import Calendar from "@/components/widgets/Calendar";
 import Tasks from "@/components/widgets/Tasks";
 import Dino from "@/components/widgets/Dino";
 
+const LeftColumnWidgets = () => (
+  <>
+    <Calendar />
+    <Weather zipCode="10001" />
+  </>
+);
+
+const CenterColumnWidgets = () => (
+  <>
+    <Search />
+    <Dino />
+  </>
+);
+
+const RightColumnWidgets = () => (
+  <>
+    <Bookmarks
+      groups={[
+        {
+          title: "Social",
+          color: "blue",
+          links: [
+            { title: "Twitter", url: "https://twitter.com" },
+            { title: "GitHub", url: "https://github.com" },
+          ],
+        },
+        {
+          title: "News",
+          color: "green",
+          links: [
+            { title: "Hacker News", url: "https://news.ycombinator.com" },
+            { title: "Reddit", url: "https://reddit.com" },
+          ],
+        },
+      ]}
+    />
+    <Tasks />
+  </>
+);
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-5 p-5 min-h-screen">
@@ -13,35 +53,13 @@ export default function Home() {
       </header>
       <main className="grid grid-cols-4 gap-8 flex-grow">
         <aside className="col-span-1 flex flex-col gap-8">
-          <Calendar />
-          <Weather zipCode="10001" />
+          <LeftColumnWidgets />
         </aside>
         <section className="col-span-2 flex flex-col gap-8">
-          <Search />
-          <Dino />
+          <CenterColumnWidgets />
         </section>
         <aside className="col-span-1 flex flex-col gap-8">
-          <Bookmarks
-            groups={[
-              {
-                title: "Social",
-                color: "blue",
-                links: [
-                  { title: "Twitter", url: "https://twitter.com" },
-                  { title: "GitHub", url: "https://github.com" },
-                ],
-              },
-              {
-                title: "News",
-                color: "green",
-                links: [
-                  { title: "Hacker News", url: "https://news.ycombinator.com" },
-                  { title: "Reddit", url: "https://reddit.com" },
-                ],
-              },
-            ]}
-          ></Bookmarks>
-          <Tasks />
+          <RightColumnWidgets />
         </aside>
       </main>
     </div>
